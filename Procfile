3c7f654eee1e7d38ebf0d0a388c5fc768e0f1844
Shubham Ch <sch@portware.com>
2019-11-16 01:29:05 +0530

    prepare for heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python bse_bhav/server.py
--- bse_bhav/config.py
+++ bse_bhav/config.py
@@ -15,8 +15,8 @@ def get_redis_connection():
     Return a redis connection
     """
     try:
-        #redis_connection = redis.from_url(os.environ.get("REDIS_URL"), charset="utf-8", decode_responses=True, db=0)
-        redis_connection = redis.Redis("localhost", charset="utf-8", decode_responses=True)
+        redis_connection = redis.from_url(os.environ.get("REDIS_URL"), charset="utf-8",
+                                          decode_responses=True, db=0)
         redis_connection.ping()
         return redis_connection
     except redis.exceptions.ConnectionError as exception:
